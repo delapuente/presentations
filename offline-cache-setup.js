e2118369403bce6cbe3e5cad665afe9ff16a4ce4
Salvador de la Puente <salva@unoyunodiez.com>
2015-02-05 11:22:13 +0100

    Moving root out of the library

--- offline-cache-setup.js
+++ offline-cache-setup.js
@@ -1,10 +1,12 @@
 
-var root = '/presentations/';
+(function () {
+  var root = document.currentScript.dataset.root || '/';
 
-navigator.serviceWorker.register(root + 'offline-cache.js', {
-  scope: root
-}).then(function () {
-  console.log('Offline cache installed at ' + new Date() + '!');
-}, function (reason) {
-  console.log(reason);
-});
+  navigator.serviceWorker.register(root + 'offline-cache.js', {
+    scope: root
+  }).then(function () {
+    console.log('Offline cache installed at ' + new Date() + '!');
+  }, function (reason) {
+    console.log(reason);
+  });
+}());
