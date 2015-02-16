1e79ae7584f588cf3bb6d10a34790df93f96d3cb
Salvador de la Puente <salva@unoyunodiez.com>
2015-02-16 17:11:46 +0100

    Update offliner

--- offliner-plugins/asyncStoragePromise.js
+++ offliner-plugins/asyncStoragePromise.js
@@ -1,14 +1,19 @@
 (function (asyncStorage) {
   for (var method in this.asyncStorage) {
     var fn = asyncStorage[method];
-    asyncStorage[method] = function (fn) {
+    asyncStorage[method] = getPromiseBasedVersion(fn);
+  }
+
+  function getPromiseBasedVersion(fn) {
+    return function () {
       var args = Array.prototype.slice.call(arguments);
       return new Promise(function (accept, reject) {
-        args[args.length - 1] = accept;
+        args[Math.max(args.length, fn.length) - 1] = accept;
         fn.apply(asyncStorage, args);
       });
-    }.bind(this, fn);
+    };
   }
+
   asyncStorage.get = asyncStorage.getItem;
   asyncStorage.set = asyncStorage.setItem;
 }(this.asyncStorage));
